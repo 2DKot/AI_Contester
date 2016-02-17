bd199b0ea1cc4b8509616c9e09c67a94efda12fe
BeloCraft <djrft@yandex.ru>
2016-02-17 20:43:00 +0300

    java-gradle Hello World
    Created on the basis of gradle

---
+++ game-server/src/main/java/com/belocarft/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.belocraft;
+
+import java.util.Scanner;
+/**
+ *
+ * @author Eugene
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+				
+        
+        System.out.print("Hello world, beaches!");				
+        Scanner a = new Scanner(System.in);    
+        
+        String n = a.nextLine();        
+    }
+    
+}
